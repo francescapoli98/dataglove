0b81356b5ea1056ccb9d71520908b0c7ae470e9a
francescapoli98 <francescapoli1998@gmail.com>
2025-04-14 11:15:01 +0200

    commit

--- glove_rospy.py
+++ glove_rospy.py
@@ -2,7 +2,11 @@
 import rospy
 import serial
 import numpy as np
+import math
+import threading
+import transforms3d.euler as euler
 from sensor_msgs.msg import JointState
+
 
 class GloveNode:
     def __init__(self):
@@ -42,11 +46,129 @@ class GloveNode:
         except serial.SerialException as e:
             rospy.logerr(f"Serial error: {e}")
         
+    #### THIS I GOT FROM LIBRARY METHODS ####
+
+    @staticmethod
+    def quat2rpy(quat):
+        """
+        Compute roll pitch and yaw angles from quaternion values (radians)
+        """
+        # TODO R and P are flipped vs the original C++ version!!!!!
+        return np.degrees(euler.quat2euler(quat, "sxyz"))
+
+    @staticmethod
+    def compute_rpy(quat):
+        # original from cpp sources
+
+        rpy = np.zeros(3, dtype=np.float32)
+
+        ratio = 180.0 / math.pi
+        rpy[0] = -ratio * math.atan2(
+            2.0 * (quat[0] * quat[1] + quat[2] * quat[3]),
+            1.0 - 2.0 * (quat[1] ** 2 + quat[2] ** 2),
+        )
+        rpy[1] = -ratio * math.asin(2.0 * (quat[0] * quat[2] - quat[3] * quat[1]))
+        rpy[2] = ratio * math.atan2(
+            2.0 * (quat[0] * quat[3] + quat[1] * quat[2]),
+            1.0 - 2.0 * (quat[2] ** 2 + quat[3] ** 2),
+        )
+        return rpy
+
+    @property
+    def rpy_wrist(self):
+        """
+        Get a copy of the wrist orientation in euler angles in degrees
+        """
+        with self.lock:
+            return self.quat2rpy(self.__quat_wrist)
+
+    @property
+    def rpy_hand(self):
+        """
+        Get a copy of the hand orientation in euler angles in degrees
+        """
+        with self.lock:
+            return self.quat2rpy(self.__quat_hand)
+
+    def reset_new_packet(self):
+        """
+        Reset new available packet flag
+        """
+        with self.lock:
+            self.__new_packet_available = False
+
+    def is_new_packet_available(self):
+        with self.lock:
+            return self.__new_packet_available
+
+    @property
+    def packet_tick(self):
+        with self.lock:
+            return self.__packet_tick
+
     def decode_sensor_values(self, data): 
         """Decodes raw data from the glove into sensor values."""
-        # Placeholder: Convert data to meaningful values
-        # return np.random.rand(len(self.joint_state.name)).tolist()  # Replace with real decoding
-        
+        # Convert data
+        """
+        Get sensor value
+        :param index index of the requested finger
+        :return sensor measure
+        :raises ValueError
+        """
+
+        if 0 <= index < 23:
+            raise ValueError("No such sensor")
+
+        with self.lock:
+            return self.__sensors[index]
+
+    @property
+    def sensors(self):
+        """
+        Get a copy of the sensors array
+        """
+        with self.lock:
+            return np.copy(self.__sensors)
+
+    @property
+    def quaternion_wrist(self):
+        """
+        Get a copy of the wrist orientation in quaternion
+        """
+        with self.lock:
+            return np.copy(self.__quat_wrist)
+
+    @property
+    def quaternion_hand(self):
+        """
+        Get a copy of the hand orientation in quaternion
+        """
+        with self.lock:
+            return np.copy(self.__quat_hand)
+
+    def __print_values(self):
+        print("Package Tick:", self.__packet_tick)
+        print("Thumb values:", self.__sensors[ThumbPh1R], self.__sensors[ThumbPh2R])
+        print("Index values:", self.__sensors[IndexPh1R], self.__sensors[IndexPh2R])
+        print("Middle values:", self.__sensors[MiddlePh1R], self.__sensors[MiddlePh2R])
+        print("Ring values:", self.__sensors[RingPh1R], self.__sensors[RingPh2R])
+        print("Little values:", self.__sensors[LittlePh1R], self.__sensors[LittlePh2R])
+
+        rpy_wrist = self.quat2rpy(self.__quat_wrist)
+        print(
+            f"Wrist rpy: " f"{rpy_wrist[0]:.3f} {rpy_wrist[1]:.3f} {rpy_wrist[2]:.3f}"
+        )
+        rpy_hand = self.quat2rpy(self.__quat_hand)
+        print(f"Hand rpy: " f"{rpy_hand[0]:.3f} {rpy_hand[1]:.3f} {rpy_hand[2]:.3f}")
+
+        print(
+            f"Wrist quat: "
+            f"{self.__quat_wrist[0]:.3f} {self.__quat_wrist[1]:.3f} {self.__quat_wrist[2]:.3f} {self.__quat_wrist[3]:.3f}"
+        )
+        print(
+            f"Hand quat: "
+            f"{self.__quat_hand[0]:.3f} {self.__quat_hand[1]:.3f} {self.__quat_hand[2]:.3f} {self.__quat_hand[3]:.3f}"
+        )        
     
     def run(self):
         rate = rospy.Rate(self.pub_rate)
